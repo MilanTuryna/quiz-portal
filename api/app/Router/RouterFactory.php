2071e24001b6c12547db1c276384e14692cc8753
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-22 20:15:42 +0100

    Implemented it in RouterFactory

--- api/app/Router/RouterFactory.php
+++ api/app/Router/RouterFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Router;
 
+use Contributte\ApiRouter\ApiRoute;
 use Nette;
 use Nette\Application\Routers\RouteList;
 
@@ -12,10 +13,23 @@ final class RouterFactory
 {
 	use Nette\StaticClass;
 
+    /**
+     * @ApiRoute()
+     * Simple route with matching (only if methods below exist):
+     * 	GET     => UsersPresenter::actionRead()
+     * 	POST    => UsersPresenter::actionCreate()
+     * 	PUT     => UsersPresenter::actionUpdate()
+     * 	DELETE  => UsersPresenter::actionDelete()
+     */
+
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = new ApiRoute("/users/find/<id>", "Users:find");
+        $router[] = new ApiRoute("/users/find/<id>/reviews", "Users:findCategories");
+        $router[] = new ApiRoute("/users/find/<id>/quizzes", "Users:findQuizzes");
+        $router[] = new ApiRoute("/users/all/<pagination>", "Users:all");
+        $router[] = new ApiRoute("/users/new", "Users:new");
 		return $router;
 	}
 }
