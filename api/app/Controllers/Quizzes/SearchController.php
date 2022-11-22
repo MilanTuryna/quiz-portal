16dab155989435b5a56d169d46d8f59d5b2ce2f0
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-22 20:47:12 +0100

    file structural changes (again)

---
+++ api/app/Controllers/Quizzes/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controllers\Quizzes;
+
+
+use Nette\Application\UI\Presenter;
+
+/**
+ * Class SearchController
+ * @package App\Controllers\Quizzes
+ */
+class SearchController extends Presenter
+{
+    public function actionRead(string $name) { // it possible to have more results (check lengths if using)
+
+    }
+}
---
+++ api/app/Controllers/Users/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controllers\Users;
+
+
+use Nette\Application\UI\Presenter;
+
+/**
+ * Class SearchController
+ * @package App\Controllers\Users
+ */
+class SearchController extends Presenter
+{
+    /**
+     * @param string $username
+     */
+    public function actionRead(string $username) { // možnost více výsledků
+
+    }
+}
