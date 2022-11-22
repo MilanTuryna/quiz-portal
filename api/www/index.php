83c9f731a83000a36c47bdab42d744918973ba75
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-22 15:53:41 +0100

    Removed apitte (i will use api-router probably)

--- api/www/index.php
+++ api/www/index.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-use Apitte\Core\Application\IApplication;
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
-$application = $container->getByType(IApplication::class);
+$application = $container->getByType(Nette\Application\Application::class);
 $application->run();
