b494826153ded63c902b0f56dc13e2c91cbfd26e
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-22 15:24:49 +0100

    Using apitte interface in entrypoint

--- api/www/index.php
+++ api/www/index.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Apitte\Core\Application\IApplication;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
-$application = $container->getByType(Nette\Application\Application::class);
+$application = $container->getByType(IApplication::class);
 $application->run();
