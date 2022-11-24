3a6ad72b03403154e0b58baea8d45668b8a5481a
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-24 11:15:03 +0100

    annotation changes

--- api/app/ElasticSearch/Repository/QuizzesRepository.php
+++ api/app/ElasticSearch/Repository/QuizzesRepository.php
@@ -4,6 +4,10 @@ namespace App\ElasticSearch\Repository;
 
 use App\ElasticSearch\AbstractRepository;
 
+/**
+ * Class QuizzesRepository
+ * @package App\ElasticSearch\Repository
+ */
 class QuizzesRepository extends AbstractRepository
 {
 
