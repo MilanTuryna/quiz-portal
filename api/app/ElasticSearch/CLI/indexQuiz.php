bba6fec8cbc1ecb0335cb5187e164e13f4ce996b
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-24 19:41:21 +0100

    Created PHP cli script for pushing mysql datas by configuration into elastic search (CLI/indexQuiz - TODO: change name because it's work for any indexes)

--- api/app/ElasticSearch/CLI/indexQuiz.php
+++ api/app/ElasticSearch/CLI/indexQuiz.php
@@ -5,6 +5,5 @@ use App\Bootstrap;
 
 $configurator = Bootstrap::boot();
 $indexer = $configurator->createContainer()->getService("elasticSearch.tableIndexer");
-$table = 'quiz';
-$indexer->setIndexName($table);
-dump($indexer->indexAll($table));
+
+dump($indexer->indexAll('quiz'));
