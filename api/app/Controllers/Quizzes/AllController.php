cc956b4771bf3dc8172255889dcaf792f452ce34
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-25 22:17:40 +0100

    I found the solution! Yes! Created actionRead

--- api/app/Controllers/Quizzes/AllController.php
+++ api/app/Controllers/Quizzes/AllController.php
@@ -3,6 +3,11 @@
 namespace App\Controllers\Quizzes;
 
 use App\Controllers\BaseController;
+use App\Database\Repository\QuizRepository;
+use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
+use Nette\Application\AbortException;
+use Nette\Database\Explorer;
 
 /**
  * Class AllController
@@ -10,10 +15,38 @@ use App\Controllers\BaseController;
  */
 class AllController extends BaseController
 {
-    /**
-     * @param string|null $orderBy
-     */
-    public function actionRead(?string $orderBy = null) {
+    private QuizRepository $quizRepository;
 
+    /**
+     * AllController constructor.
+     * @param ResponseFormatter $formatter
+     * @param Explorer $explorer
+     * @param QuizRepository $quizRepository
+     */
+    public function __construct(ResponseFormatter $formatter, Explorer $explorer, QuizRepository $quizRepository)
+    {
+        parent::__construct($formatter, $explorer);
+
+        $this->quizRepository = $quizRepository;
+    }
+
+    /**
+     * @param int $page
+     * @param string|null $order
+     * @throws AbortException
+     */
+    public function actionRead(int $page, ?string $order = null): void {
+        $quizzes = $this->quizRepository->findAll($order);
+
+        $lastPage = 0;
+        $responseContent = $this->formatter->formatContent([
+            "results" => array_values(array_map(fn($activeRow) => $activeRow->toArray(), $quizzes->page($page, 30, $lastPage)->fetchAll())),
+            "pagination" => [
+                "page" => $page,
+                "lastPage" => $lastPage
+            ]
+        ],200);
+        $response = new JsonResponse($responseContent, 200, true);
+        $this->sendResponse($response);
     }
 }
