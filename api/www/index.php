959da01ea72f39dc6a9b7979adad33e7602bc842
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-26 19:18:23 +0100

    Exception handler in www/index.php to ensure that API works in every situation (connection problems, database down...) and provide information about it. (easier for frontend)

--- api/www/index.php
+++ api/www/index.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Http\ResponseFormatter;
+
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
 $application = $container->getByType(Nette\Application\Application::class);
-$application->run();
+try {
+    $application->run();
+} catch (Exception $e) {
+    $message = $e instanceof \Nette\Database\ConnectionException ? \App\Http\Exceptions\Messages::CONNECTION_EXCEPTION : "";
+    /**
+     * @var ResponseFormatter $responseFormatter
+     */
+    $responseFormatter = $container->getService("responseFormatter");
+    $formattedResponse = $responseFormatter->formatException(new Exception($message, 500), 500);
+    http_response_code(500);
+    die(\Nette\Utils\Json::encode($formattedResponse, \Nette\Utils\Json::PRETTY));
+}
