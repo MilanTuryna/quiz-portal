76714e89c355c51419ef202cf0dd389d21efee9e
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-26 19:20:14 +0100

    Created Exceptions\Messages (just constant class for save all available return message)

---
+++ api/app/Http/Exceptions/Messages.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Exceptions;
+
+/**
+ * Class Messages
+ * @package App\Http\Exceptions
+ */
+final class Messages
+{
+    const BAD_INPUT_REQUEST = "BAD_INPUT_REQUEST";
+    const CONNECTION_EXCEPTION = "CONNECTION_EXCEPTION";
+}
