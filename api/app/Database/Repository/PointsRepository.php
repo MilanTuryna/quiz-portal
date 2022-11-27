c0e2789f1c4f67887e2c91d2206a430805b3493b
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-27 18:26:13 +0100

    PointsRepository

--- api/app/Database/Repository/PointsRepository.php
+++ api/app/Database/Repository/PointsRepository.php
@@ -1,7 +1,23 @@
 <?php
 
+namespace App\Database\Repository;
 
-class PointsRepository
+use App\Database\Repository;
+use App\Database\Table;
+use Nette\Database\Explorer;
+
+/**
+ * Class PointsRepository
+ * @package App\Database\Repository
+ */
+class PointsRepository extends Repository
 {
-
+    /**
+     * PointsRepository constructor.
+     * @param Explorer $explorer
+     */
+    public function __construct(Explorer $explorer)
+    {
+        parent::__construct(Table::POINTS, $explorer);
+    }
 }
