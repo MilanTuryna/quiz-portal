0f5adab7fde2b2f4031d9a48f584cc4532b85037
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-27 18:18:44 +0100

    Created search method for UserRepoistory

--- api/app/Controllers/Users/SearchController.php
+++ api/app/Controllers/Users/SearchController.php
@@ -5,6 +5,11 @@ namespace App\Controllers\Users;
 
 
 use App\Controllers\BaseController;
+use App\Database\Repository\UserRepository;
+use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
+use Nette\Application\AbortException;
+use Nette\Database\Explorer;
 
 /**
  * Class SearchController
@@ -12,10 +17,30 @@ use App\Controllers\BaseController;
  */
 class SearchController extends BaseController
 {
+    public UserRepository $repository;
+
+    /**
+     * SearchController constructor.
+     * @param ResponseFormatter $formatter
+     * @param Explorer $explorer
+     * @param UserRepository $repository
+     */
+    public function __construct(ResponseFormatter $formatter, Explorer $explorer, UserRepository $repository)
+    {
+        parent::__construct($formatter, $explorer);
+
+        $this->repository = $repository;
+    }
+
     /**
      * @param string $username
+     * @throws AbortException
      */
     public function actionRead(string $username) { // možnost více výsledků
-
+        $code = 200;
+        $search = $this->repository->search($username);
+        $content = $this->formatter->formatContent($search, $code);
+        $response = new JsonResponse($content, $code, true);
+        $this->sendResponse($response);
     }
 }
--- api/app/Database/Repository/UserRepository.php
+++ api/app/Database/Repository/UserRepository.php
@@ -2,8 +2,10 @@
 
 namespace App\Database\Repository;
 
+use App\Database\Entity\User;
 use App\Database\Repository;
 use App\Database\Table;
+use App\ElasticSearch\ElasticManager;
 use Nette\Database\Explorer;
 
 /**
@@ -12,12 +14,28 @@ use Nette\Database\Explorer;
  */
 class UserRepository extends Repository
 {
+    private ElasticManager $elasticManager;
+
     /**
      * UserRepository constructor.
      * @param Explorer $explorer
+     * @param ElasticManager $elasticManager
      */
-    public function __construct(Explorer $explorer)
+    public function __construct(Explorer $explorer, ElasticManager $elasticManager)
     {
         parent::__construct(Table::USER, $explorer);
+
+        $this->elasticManager = $elasticManager;
+    }
+
+    /**
+     * @param string $nickname
+     * @return array
+     */
+    public function search(string $nickname): array {
+        if($this->elasticManager->getClient()->isEnabled()) return $this->elasticManager->multiMatch(Table::USER, $nickname, [User::nickname], 2);
+        // without password, email (TODO: email only when user is authorized and it is him)
+        $search = $this->explorer->query("SELECT nickname, date_created, hideFinishedQuizzes, points, id  FROM " . Table::USER . " WHERE match(". User::name .") AGAINST(?)", [$nickname])->fetchAll();
+        return ["results" => $search];
     }
 }
