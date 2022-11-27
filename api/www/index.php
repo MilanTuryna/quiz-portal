04ce2ea4c13f673f3535950310d786449eb27801
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-27 12:46:57 +0100

    Ok, it's some chaotic code in index.php for anyone who isn't interested but it's working

--- api/www/index.php
+++ api/www/index.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Http\Exceptions\HttpClientException;
 use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
+use Nette\Http\IResponse;
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
@@ -12,12 +15,25 @@ $application = $container->getByType(Nette\Application\Application::class);
 try {
     $application->run();
 } catch (Exception $e) {
-    $message = $e instanceof \Nette\Database\ConnectionException ? \App\Http\Exceptions\Messages::CONNECTION_EXCEPTION : "";
-    /**
-     * @var ResponseFormatter $responseFormatter
-     */
-    $responseFormatter = $container->getService("responseFormatter");
-    $formattedResponse = $responseFormatter->formatException(new Exception($message, 500), 500);
-    http_response_code(500);
-    die(\Nette\Utils\Json::encode($formattedResponse, \Nette\Utils\Json::PRETTY));
+    if($configurator->isDebugMode()) {
+        throw $e;
+    } else {
+        /**
+         * @var ResponseFormatter $responseFormatter
+         */
+        $responseFormatter = $container->getService("responseFormatter");
+        if(in_array($e->getCode(), array_keys(IResponse::REASON_PHRASES))) {
+            $httpCode = $e->getCode();
+            $message = IResponse::REASON_PHRASES[$httpCode];
+        } else {
+            $httpCode = 500;
+            $message = "";
+        }
+        $formattedResponse = $responseFormatter->formatException(new HttpClientException($message, new $e, (int)$e->getCode()), $httpCode);
+        $jsonResponse = new JsonResponse($formattedResponse, $httpCode, true, null);
+        http_response_code($httpCode);
+        header("Content-Type: application/json; charset=UTF-8");
+        echo $jsonResponse;
+        die();
+    }
 }
