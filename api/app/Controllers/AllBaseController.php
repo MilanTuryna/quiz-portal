f0f6207390bfb3e5a92126ad37de70625d524f2b
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-27 15:35:17 +0100

    Created AllBaseController

---
+++ api/app/Controllers/AllBaseController.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use App\Database\Repository;
+use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
+use Nette\Application\AbortException;
+use Nette\Database\Explorer;
+
+/**
+ * Class AllBaseController
+ * @package App\Controllers
+ */
+class AllBaseController extends BaseController
+{
+    protected Repository $repository;
+
+    /**
+     * AllBaseController constructor.
+     * @param ResponseFormatter $formatter
+     * @param Explorer $explorer
+     * @param Repository $repository
+     */
+    public function __construct(ResponseFormatter $formatter, Explorer $explorer, Repository $repository)
+    {
+        parent::__construct($formatter, $explorer);
+
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param int $page
+     * @param string|null $order
+     * @throws AbortException
+     */
+    public function actionRead(int $page, ?string $order = null): void {
+        $quizzes = $this->repository->findAll($order);
+
+        $lastPage = 0;
+        $responseContent = $this->formatter->formatContent([
+            "results" => array_values(array_map(fn($activeRow) => $activeRow->toArray(), $quizzes->page($page, 30, $lastPage)->fetchAll())),
+            "pagination" => [
+                "page" => $page,
+                "lastPage" => $lastPage
+            ]
+        ],200);
+        $response = new JsonResponse($responseContent, 200, true);
+        $this->sendResponse($response);
+    }
+}
