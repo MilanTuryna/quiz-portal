10a8aee554a6f909e5800a9c0fa3fefadb02953d
MilanTuryna <miloslavturyna@seznam.cz>
2022-11-27 15:30:02 +0100

    Created method formatRawBody Http\ResponseFormatter

--- api/app/Http/ResponseFormatter.php
+++ api/app/Http/ResponseFormatter.php
@@ -3,6 +3,10 @@
 namespace App\Http;
 
 use App\Http\Exceptions\HttpClientException;
+use Nette\Http\Request;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
+
 /**
  * Class ResponseFormatter
  * @package App\Http
@@ -38,5 +42,14 @@ final class ResponseFormatter
         $exceptionName = end($splitClassName);
         $message = $clientException->getMessage() === "" ? $exceptionName : $clientException->getMessage();
         return $this->formatContent(["error" => ["message" => $message, "code" => $clientException->getCode(), "exception" => $exceptionName]], $code, self::STATUS_ERROR);
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     * @throws JsonException
+     */
+    public function formatRawBody(Request $request): array {
+        return Json::decode($request->getRawBody());
     }
 }
