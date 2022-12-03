935d25eee30b2dd500a0f360c9a1e16408630035
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-03 18:24:20 +0100

    Added findAll for UserRepository (same as parent findAll but this without password)

--- api/app/Database/Repository/UserRepository.php
+++ api/app/Database/Repository/UserRepository.php
@@ -7,6 +7,7 @@ use App\Database\Repository;
 use App\Database\Table;
 use App\ElasticSearch\ElasticManager;
 use Nette\Database\Explorer;
+use Nette\Database\Table\Selection;
 
 /**
  * Class UserRepository
@@ -37,5 +38,14 @@ class UserRepository extends Repository
         // without password, email (TODO: email only when user is authorized and it is him)
         $search = $this->explorer->query("SELECT nickname, date_created, hideFinishedQuizzes, points, id  FROM " . Table::USER . " WHERE match(". User::name .") AGAINST(?)", [$nickname])->fetchAll();
         return ["results" => $search];
+    }
+
+    /**
+     * @param string|null $orderQuery
+     * @return Selection
+     */
+    public function findAll(?string $orderQuery = null): Selection
+    {
+        return $this->findAll()->select("nickname, date_created, hideFinishedQuizzes, points, id");
     }
 }
