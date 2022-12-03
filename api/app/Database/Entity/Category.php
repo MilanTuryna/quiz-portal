d0d8331fb3e52fbd0eed8f347a9e50a8efbe2295
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-03 17:38:55 +0100

    Created Category::url ($url)

--- api/app/Database/Entity/Category.php
+++ api/app/Database/Entity/Category.php
@@ -10,11 +10,13 @@ namespace App\Database\Entity;
 class Category
 {
     const name = "name";
+    const url = "url";
     const level = "level";
     const parent_id = "parent_id";
     const id = "id";
 
     public string $name;
+    public string $url;
     public int $level;
     public int $parent_id;
     public int $id;
