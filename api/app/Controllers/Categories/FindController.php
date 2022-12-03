00d3bc9462145faf15ee28556cb1c5c7cc8c1b9f
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-03 17:26:30 +0100

    Added actionRead and replacing abstract copy of this method (checking if is int, if not get row by category name)

--- api/app/Controllers/Categories/FindController.php
+++ api/app/Controllers/Categories/FindController.php
@@ -3,8 +3,11 @@
 namespace App\Controllers\Categories;
 
 use App\Controllers\FindBaseController;
+use App\Database\Entity\Category;
 use App\Database\Repository;
 use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
+use Nette\Application\AbortException;
 use Nette\Database\Explorer;
 
 /**
@@ -22,5 +25,18 @@ class FindController extends FindBaseController
     public function __construct(ResponseFormatter $formatter, Explorer $explorer, Repository\CategoryRepository $repository)
     {
         parent::__construct($formatter, $explorer, $repository);
+    }
+
+    /**
+     * @param $id
+     * @throws AbortException
+     */
+    public function actionRead($id): void {
+        $row = is_int($id) ? $this->repository->findById($id) : $this->repository->findByColumn(Category::name, $id);
+        $data = $row ? $row->toArray() : [];
+        $code = $row ? 200 : 404;
+        $content = $this->formatter->formatContent($data, $code);
+        $response = new JsonResponse($content, $code, true);
+        $this->sendResponse($response);
     }
 }
