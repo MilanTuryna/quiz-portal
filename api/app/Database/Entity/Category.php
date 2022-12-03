747c6b9070998b9945785b3f826e2c26408dc917
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-03 17:25:18 +0100

    Added columns to Entity\Category

--- api/app/Database/Entity/Category.php
+++ api/app/Database/Entity/Category.php
@@ -3,8 +3,19 @@
 
 namespace App\Database\Entity;
 
-
+/**
+ * Class Category
+ * @package App\Database\Entity
+ */
 class Category
 {
+    const name = "name";
+    const level = "level";
+    const parent_id = "parent_id";
+    const id = "id";
 
+    public string $name;
+    public int $level;
+    public int $parent_id;
+    public int $id;
 }
