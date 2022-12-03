5239d5cf4a50d3eeb18fac0dd67d47c1723d6b8a
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-03 21:17:58 +0100

    Added pagination to actionRead

--- api/app/Controllers/FindRelatedController.php
+++ api/app/Controllers/FindRelatedController.php
@@ -16,6 +16,11 @@ use Nette\Database\Explorer;
  */
 class FindRelatedController extends BaseController
 {
+    /**
+     * FindRelatedController constructor.
+     * @param ResponseFormatter $formatter
+     * @param Explorer $explorer
+     */
     public function __construct(ResponseFormatter $formatter, Explorer $explorer)
     {
         parent::__construct($formatter, $explorer);
@@ -25,15 +30,25 @@ class FindRelatedController extends BaseController
      * @param string $table
      * @param string $id
      * @param string $related
-     * @throws AbortException|BadRequestException
+     * @param int|null $page
+     * @throws AbortException
+     * @throws BadRequestException
      */
-    public function actionRead(string $table, string $id, string $related) {
+    public function actionRead(string $table, string $id, string $related, ?int $page = null) {
         if(!array_key_exists($table, Table::ROUTER_TO_TABLE) || !array_key_exists($related, Table::ROUTER_TO_TABLE)) $this->error();
         $table = Table::ROUTER_TO_TABLE[$table];
         $related = Table::ROUTER_TO_TABLE[$related];
         $foreignKey = Table::FOREIGN_KEYS[$table];
-        $rows = $this->explorer->table($related)->where($foreignKey . " = ?", $id)->fetchAll();
-        $content = $this->formatter->formatContent(Table::fetchAllToArray($rows), 200);
+        $rows = $this->explorer->table($related)->where($foreignKey . " = ?", $id);
+        $lastPage = null;
+        $content = $this->formatter->formatContent([
+            "results" => Table::fetchAllToArray($page ? $rows->page($page, 30, $lastPage)->fetchAll() : $rows->fetchAll()),
+            "pagination" => $page ? [
+                "page" => $page,
+                "lastPage" => $lastPage,
+                "pageExist" => !($page > $lastPage)
+            ] : null
+        ], 200);
         $response = new JsonResponse($content, 200, true);
         $this->sendResponse($response);
     }
