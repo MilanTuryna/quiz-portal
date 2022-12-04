f39f5fc0458f5755501a58bb3d5848a7604ac8f0
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-04 13:11:52 +0100

    Deleted findTags from router

--- api/app/Router/RouterFactory.php
+++ api/app/Router/RouterFactory.php
@@ -40,7 +40,6 @@ final class RouterFactory
         $router[] = new ApiRoute("/categories/all[/<page>]", "Categories:all");
         $router[] = new ApiRoute("/categories/find/<id>", "Categories:find"); // it's possible to send category name instead id
 
-        $router[] = new ApiRoute("/quizzes/find/<id>/tags", "Quizzes:findTags");
         $router[] = new ApiRoute("/<table>/find/<id>/<related>[/<page>]", "FindRelated");
 
         $router->withModule("Quizzes") // ApiRoute doesn't support spaces (%20) and some special characters in URL
