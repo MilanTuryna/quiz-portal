93b1dfd2ecb18e9aa5d15a8fe1485acd83bcdb9d
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-04 14:39:15 +0100

    Created findAll method that using group by "tag" col in TagRepository

--- api/app/Database/Repository/TagRepository.php
+++ api/app/Database/Repository/TagRepository.php
@@ -5,6 +5,7 @@ namespace App\Database\Repository;
 use App\Database\Repository;
 use App\Database\Table;
 use Nette\Database\Explorer;
+use Nette\Database\Table\Selection;
 
 /**
  * Class TagRepository
@@ -28,5 +29,16 @@ class TagRepository extends Repository
     public function findByQuiz(int $id): array
     {
         return $this->findAll()->select("tag, id")->where(Table::FOREIGN_KEYS[Table::QUIZ] . " = ?", $id)->fetchAll();
+    }
+
+    /**
+     * @param string|null $orderQuery
+     * @param array|null $select
+     * @param bool $includesPrivate
+     * @return Selection
+     */
+    public function findAll(?string $orderQuery = null, ?array $select = null, bool $includesPrivate = false): Selection
+    {
+        return parent::findAll()->group("tag");
     }
 }
