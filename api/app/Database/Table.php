dd506294ec72f6645f2c996411a1e8e5a66d314b
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-04 14:38:23 +0100

    Created Table::ALLOWED_VALUES[Table::TAG]

--- api/app/Database/Table.php
+++ api/app/Database/Table.php
@@ -69,6 +69,9 @@ class Table
             User::date_created,
             User::hideFinishedQuizzes,
             User::points,
+        ],
+        Table::TAG => [
+            Tag::tag,
         ]
     ];
 
