453eecdf50b1dbaf672c428e5a8b424f87fe4d24
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-04 14:48:34 +0100

    Allowing non-numeric ids (Tag::name) in Tags\FindController

--- api/app/Controllers/Tags/FindController.php
+++ api/app/Controllers/Tags/FindController.php
@@ -1,11 +1,13 @@
 <?php
 
 
-namespace App\Categories\AllController;
+namespace App\Controllers\Tags;
 
 use App\Controllers\FindBaseController;
+use App\Database\Entity\Tag;
 use App\Database\Repository;
 use App\Http\ResponseFormatter;
+use App\Http\Responses\JsonResponse;
 use Nette\Database\Explorer;
 
 /**
@@ -23,5 +25,15 @@ class FindController extends FindBaseController
     public function __construct(ResponseFormatter $formatter, Explorer $explorer, Repository\TagRepository $repository)
     {
         parent::__construct($formatter, $explorer, $repository);
+    }
+
+    public function actionRead($id): void
+    {
+        $row = is_numeric($id) ? $this->repository->findById($id) : $this->repository->findByColumn(Tag::tag, $id);
+        $data = $row ? $row->toArray() : new \stdClass();
+        $code = $row ? 200 : 404;
+        $content = $this->formatter->formatContent($data, $code);
+        $response = new JsonResponse($content, $code, true);
+        $this->sendResponse($response);
     }
 }
