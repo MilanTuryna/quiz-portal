77213b24fbb69bc8ca1162f8b1cb28a7616ca30a
MilanTuryna <miloslavturyna@seznam.cz>
2022-12-04 13:42:54 +0100

    Deletted findAll method from UserRepository and use Table::ALLOWED_VALUES in search method

--- api/app/Database/Repository/UserRepository.php
+++ api/app/Database/Repository/UserRepository.php
@@ -7,7 +7,6 @@ use App\Database\Repository;
 use App\Database\Table;
 use App\ElasticSearch\ElasticManager;
 use Nette\Database\Explorer;
-use Nette\Database\Table\Selection;
 
 /**
  * Class UserRepository
@@ -36,16 +35,7 @@ class UserRepository extends Repository
     public function search(string $nickname): array {
         if($this->elasticManager->getClient()->isEnabled()) return $this->elasticManager->multiMatch(Table::USER, $nickname, [User::nickname], 2);
         // without password, email (TODO: email only when user is authorized and it is him)
-        $search = $this->explorer->query("SELECT nickname, date_created, hideFinishedQuizzes, points, id  FROM " . Table::USER . " WHERE match(". User::name .") AGAINST(?)", [$nickname])->fetchAll();
+        $search = $this->explorer->query("SELECT ". implode(",", Table::ALLOWED_VALUES[$this->table]) ." FROM " . Table::USER . " WHERE match(". User::nickname .") AGAINST(?)", [$nickname])->fetchAll();
         return ["results" => $search];
-    }
-
-    /**
-     * @param string|null $orderQuery
-     * @return Selection
-     */
-    public function findAll(?string $orderQuery = null): Selection
-    {
-        return $this->findAll()->select("nickname, date_created, hideFinishedQuizzes, points, id");
     }
 }
